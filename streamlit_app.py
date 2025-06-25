8af53e00e7d8a3b50e4eca6e256e32c4a114dc3b
mynsun <alstjs3022@naver.com>
2025-06-25 02:30:10 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -238,10 +238,16 @@ def round1(x):
 def main():
     st.title("🍽️ 식단 관리 시스템")
 
-    # 사진으로 음식 등록 버튼: 현재 페이지 내에서 이동
     if st.button("📸 사진으로 음식 등록", key="photo_upload"):
-        st.experimental_set_query_params()
-        st.experimental_redirect("http://15.164.56.89:30800/")
+    # JavaScript를 이용한 리다이렉트
+    components.html(
+        """
+        <script>
+            window.location.href = 'http://15.164.56.89:30800/';
+        </script>
+        """,
+        height=0
+    )
 
     if 'recommendations' not in st.session_state:
         st.session_state.recommendations = {}
